a61ef3b9c78665ee37e1805c5984a9e4003f46e8
Nadia <lagoanadia@gmail.com>
2026-05-13 14:15:49 +0200

    Add files via upload

---
+++ TicTacToe/script.js
@@ -0,0 +1,36 @@
+let container = document.getElementsByClassName("container");
+let fields = document.querySelectorAll("td");
+let table = document.createElement("table");
+
+let currentPlayer = "X";
+let choices = ["O","X"];
+let turn = 0;
+
+function setUp()
+{
+    for(let i = 0; i<9; i++)
+    {
+        fields[i].classList.add("unactive");  
+    }
+}
+
+//EventListener for when field is clicked
+for(let i = 0; i<9; i++)
+{
+    fields[i].addEventListener("click",()=>
+    {
+        fields[i].classList.remove("unactive"); 
+        let chosenField = fields[i];
+
+        changeTurn(event);
+    });  
+}
+
+function changeTurn(event)
+{
+    turn = 1 - turn; //switches between 0 and 1
+    currentPlayer = choices[turn];
+    for
+///// for para cambiar todas las fields a turn + un array de chosen fields !
+}
+
